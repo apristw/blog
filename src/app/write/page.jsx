b97bbb1d9fcd52c67e5557df2d52291edb4f2183
apristw <mailapriawan@gmail.com>
2024-06-06 22:39:16 +0700

    add write page + install React Quill for text area

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import styles from "./writePage.module.css";
+import Image from "next/image";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
 function WritePage() {
-	return <h1>Add New Post</h1>;
+	const [open, setOpen] = useState(false);
+	const [value, setValue] = useState("");
+	return (
+		<div className={styles.container}>
+			<input type="text" placeholder="Title" className={styles.input} />
+			<div className={styles.editor}>
+				<button className={styles.button} onClick={() => setOpen(!open)}>
+					<Image src="/plus.png" alt="plus" width={16} height={16} />
+				</button>
+				{open && (
+					<div className={styles.add}>
+						<button className={styles.addButton}>
+							<Image src="/image.png" alt="image" width={16} height={16} />
+						</button>
+						<button className={styles.addButton}>
+							<Image
+								src="/external.png"
+								alt="external"
+								width={16}
+								height={16}
+							/>
+						</button>
+						<button className={styles.addButton}>
+							<Image src="/video.png" alt="video" width={16} height={16} />
+						</button>
+					</div>
+				)}
+				<ReactQuill
+					className={styles.textArea}
+					theme="bubble"
+					value={value}
+					onChange={setValue}
+					placeholder="Tell Your Story Here ..."
+				/>
+			</div>
+			<button className={styles.publish}>Publish</button>
+		</div>
+	);
 }
 
 export default WritePage;
