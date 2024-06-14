f1c1a51e3bbf914d5777beb413bdd935d5d78aaf
apristw <mailapriawan@gmail.com>
2024-06-14 10:34:12 +0700

    Create function for pagination and categories

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,17 +3,38 @@ import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Card from "../card/Card";
 
-function CardList() {
+const getData = async (page, cat) => {
+	const res = await fetch(
+		`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+		{
+			cache: "no-store",
+		}
+	);
+
+	if (!res.ok) {
+		throw new Error("Failed");
+	}
+
+	return res.json();
+};
+
+async function CardList({ page, cat }) {
+	const { posts, count } = await getData(page, cat);
+
+	const POST_PER_PAGE = 2;
+
+	const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+	const hasNext = POST_PER_PAGE * (page - 1) * POST_PER_PAGE < count;
+
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.title}>Recent Post</h1>
 			<div className={styles.post}>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+				{posts?.map((item) => {
+					return <Card item={item} key={item._id} />;
+				})}
 			</div>
-			<Pagination />
+			<Pagination page={page} hasPev={hasPrev} hasNext={hasNext} />
 		</div>
 	);
 }
